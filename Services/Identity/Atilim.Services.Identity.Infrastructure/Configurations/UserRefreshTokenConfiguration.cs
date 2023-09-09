1eac543924438f4fcdae243e732bc6e15a990d68
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-09-09 23:42:06 +0300

    - Identity tablolarının oluşturulması işlemleri. - Context nesnesinin oluşturulması.

---
+++ Services/Identity/Atilim.Services.Identity.Infrastructure/Configurations/UserRefreshTokenConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Atilim.Services.Identity.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Atilim.Services.Identity.Infrastructure.Configurations
+{
+    public class UserRefreshTokenConfiguration : IEntityTypeConfiguration<UserRefreshToken>
+    {
+        public void Configure(EntityTypeBuilder<UserRefreshToken> builder)
+        {
+            builder.HasKey(b => b.UserId);
+            builder.Property(b => b.Code).IsRequired().HasMaxLength(200);
+        }
+    }
+}
---
+++ Services/Identity/Atilim.Services.Identity.Infrastructure/IdentityContext.cs
@@ -0,0 +1,20 @@
+﻿using Atilim.Services.Identity.Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Atilim.Services.Identity.Infrastructure
+{
+    public class IdentityContext : IdentityDbContext<User, UserRole, string>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options) { }
+
+        public DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
