d51eaecd1db600fd14a20e77c0518d3383402c4f
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-09-09 23:20:26 +0300

    - BaseEntity oluşturuldu.

---
+++ Shared/Atilim.Shared/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace Atilim.Shared
+{
+    public abstract class BaseEntity
+    {
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid().ToString();
+            CreatedOn = DateTime.Now;
+        }
+
+        public string Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+        public string UpdatedBy { get; set; }
+    }
+}
