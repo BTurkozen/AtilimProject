ceb7a4a19bf800f72139ba0b11efe2d82eda0ab3
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-09-10 14:28:40 +0300

    - student api kaldırıldı. Tek api üzerinden işlemler yürütülecek.

---
+++ Services/Identity/Atilim.Services.Identity.Api/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using Atilim.Services.Identity.Application.Dtos;
+using Atilim.Services.Identity.Application.Interfaces;
+using Atilim.Shared.CustomControllerBases;
+using Atilim.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Atilim.Services.Identity.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : CustomControllerBase
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public AuthController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateToken(LoginDto loginDto)
+        {
+            return CustomActionResult(await _authenticationService.CreateTokenAsync(loginDto));
+        }
+    }
+}
---
+++ Services/Identity/Atilim.Services.Identity.Api/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Atilim.Services.Identity.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "admin")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return Ok(nameof(UsersController));
+        }
+    }
+}
