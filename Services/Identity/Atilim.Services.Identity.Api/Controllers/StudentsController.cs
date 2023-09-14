f27b55cd25f63557365b5ab366eb70fba9a588d9
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-09-14 23:23:01 +0300

    - Api tarafında  Student controller oluşturuldu. - GetAllStudent action'ı eklendi ve veri çekildi.

---
+++ Services/Identity/Atilim.Services.Identity.Api/Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+﻿using Atilim.Services.Identity.Application.Features.Queries.StudentQueries;
+using Atilim.Shared.CustomControllerBases;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Atilim.Services.Identity.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : CustomControllerBase
+    {
+        private readonly IMediator _medator;
+
+        public StudentsController(IMediator medator)
+        {
+            _medator = medator;
+        }
+
+        public async Task<IActionResult> GetAllStudent()
+        {
+            return CustomActionResult(await _medator.Send(new GetAllStudentQuery()));
+        }
+    }
+}
