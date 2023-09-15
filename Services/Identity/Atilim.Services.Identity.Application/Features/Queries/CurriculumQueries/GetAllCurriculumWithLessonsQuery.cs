644c0e83f24140446936d5447e803bbc5a836977
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-09-16 00:23:38 +0300

    - CurriculumWtihLEssons query ve hadnlerları eklendi.

---
+++ Services/Identity/Atilim.Services.Identity.Application/Features/Queries/CurriculumQueries/GetAllCurriculumWithLessonsQuery.cs
@@ -0,0 +1,33 @@
+﻿using Atilim.Services.Identity.Application.Dtos.CurriculumDtos;
+using Atilim.Services.Identity.Application.Interfaces.StudentInterfaces;
+using Atilim.Shared.Dtos;
+using MediatR;
+
+namespace Atilim.Services.Identity.Application.Features.Queries.CurriculumQueries
+{
+    public class GetAllCurriculumWithLessonsQuery : IRequest<ResponseDto<List<CurriculumWithLessonsDto>>>
+    {
+        public class GetAllCurriculumWithLessonsQueryHandler : IRequestHandler<GetAllCurriculumWithLessonsQuery, ResponseDto<List<CurriculumWithLessonsDto>>>
+        {
+            private readonly ICurriculumService _curriculumService;
+
+            public GetAllCurriculumWithLessonsQueryHandler(ICurriculumService curriculumService)
+            {
+                _curriculumService = curriculumService;
+            }
+
+            public async Task<ResponseDto<List<CurriculumWithLessonsDto>>> Handle(GetAllCurriculumWithLessonsQuery request, CancellationToken cancellationToken)
+            {
+                var curriculumWithLessonsDtos = await _curriculumService.GetAllCurriculumWithLessonsAsync();
+
+                if (curriculumWithLessonsDtos.Count > 0)
+                {
+
+                    return ResponseDto<List<CurriculumWithLessonsDto>>.Success(curriculumWithLessonsDtos, System.Net.HttpStatusCode.OK);
+                }
+
+                return ResponseDto<List<CurriculumWithLessonsDto>>.Fail("Müfredatlar bulunamadı!!!", System.Net.HttpStatusCode.NotFound);
+            }
+        }
+    }
+}
---
+++ Services/Identity/Atilim.Services.Identity.Application/Features/Queries/CurriculumQueries/GetCurriculumWithLessonsByIdQuery.cs
@@ -0,0 +1,35 @@
+﻿using Atilim.Services.Identity.Application.Dtos.CurriculumDtos;
+using Atilim.Services.Identity.Application.Interfaces.StudentInterfaces;
+using Atilim.Shared.Dtos;
+using MediatR;
+
+namespace Atilim.Services.Identity.Application.Features.Queries.CurriculumQueries
+{
+    public class GetCurriculumWithLessonsByIdQuery : IRequest<ResponseDto<CurriculumWithLessonsDto>>
+    {
+        public int Id { get; set; }
+
+        public class GetCurriculumWithLessonsByIdQueryHandler : IRequestHandler<GetCurriculumWithLessonsByIdQuery, ResponseDto<CurriculumWithLessonsDto>>
+        {
+            private readonly ICurriculumService _curriculumService;
+
+            public GetCurriculumWithLessonsByIdQueryHandler(ICurriculumService curriculumService)
+            {
+                _curriculumService = curriculumService;
+            }
+
+            public async Task<ResponseDto<CurriculumWithLessonsDto>> Handle(GetCurriculumWithLessonsByIdQuery request, CancellationToken cancellationToken)
+            {
+                var curriculumWithLessonsDto = await _curriculumService.GetCurriculumWithLessonsByIdAsync(request.Id);
+
+                if (curriculumWithLessonsDto is not null)
+                {
+                    return ResponseDto<CurriculumWithLessonsDto>.Success(curriculumWithLessonsDto, System.Net.HttpStatusCode.OK);
+                }
+
+                return ResponseDto<CurriculumWithLessonsDto>.Fail($"{request.Id} Id'li müfredat bulunamadı!!!", System.Net.HttpStatusCode.NotFound);
+
+            }
+        }
+    }
+}
