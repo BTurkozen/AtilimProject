7536da8dd250f3d9f476cfb9fc6740258281e918
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-09-15 23:10:18 +0300

    - CurriculumController içerisinde actionlar oluşturuldu.

--- Services/Identity/Atilim.Services.Identity.Api/Controllers/CurriculumsController.cs
+++ Services/Identity/Atilim.Services.Identity.Api/Controllers/CurriculumsController.cs
@@ -1,12 +1,29 @@
-﻿using Atilim.Shared.CustomControllerBases;
-using Microsoft.AspNetCore.Http;
+﻿using Atilim.Services.Identity.Application.Features.Queries.CurriculumQueries;
+using Atilim.Shared.CustomControllerBases;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Atilim.Services.Identity.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class CurriculumsController : CustomControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public CurriculumsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> GetAll() => CustomActionResult(await _mediator.Send(new GetAllCurriculumQuery()));
+
+        [HttpGet("{id}")]
+        [Authorize(Roles = "admin,student")]
+        public async Task<IActionResult> GetById(int id) => CustomActionResult(await _mediator.Send(new GetCurriculumByIdQuery() { Id = id }));
     }
 }
