f1e7e3ac8f5fc5d4204acbe18caedd625b301fe0
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-09-16 15:24:23 +0300

    - Projenin oluşturulması ve login page eklenmesi işlemleri tamamlandı.

---
+++ Presentations/Atilim.Presentations.WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Atilim.Presentations.WebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Presentations/Atilim.Presentations.WebApplication/Program.cs
@@ -0,0 +1,66 @@
+﻿using Atilim.Shared.Settings.Concrates;
+using Atilim.Shared.Settings.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var tokenConfiguration = builder.Configuration.GetSection(nameof(TokenSettings));
+
+builder.Services.Configure<TokenSettings>(tokenConfiguration);
+
+builder.Services.AddSingleton<ITokenSettings>(options =>
+{
+    return options.GetRequiredService<IOptions<TokenSettings>>().Value;
+});
+
+var tokenSettings = tokenConfiguration.Get<TokenSettings>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+}).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+    {
+        ValidIssuer = tokenSettings.Issuer,
+        ValidAudience = tokenSettings.Audience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenSettings.SecurityKey)),
+
+        ValidateIssuerSigningKey = true,
+        ValidateAudience = true,
+        ValidateIssuer = true,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero,
+    };
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Login}/{action=Index}/{id?}");
+
+app.Run();
