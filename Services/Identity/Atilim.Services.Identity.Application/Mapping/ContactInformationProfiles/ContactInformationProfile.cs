b75e04fcc51e96751993043ef2aa12912f7ac774
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-09-16 10:35:38 +0300

    - StudentIdentity ve ContactInformation için profile işlemleri yapıldı.

---
+++ Services/Identity/Atilim.Services.Identity.Application/Mapping/ContactInformationProfiles/ContactInformationProfile.cs
@@ -0,0 +1,14 @@
+﻿using Atilim.Services.Identity.Application.Dtos.ContactInformationDtos;
+using Atilim.Services.Identity.Domain.Entities.StudentEntities;
+using AutoMapper;
+
+namespace Atilim.Services.Identity.Application.Mapping.ContactInformationProfiles
+{
+    public class ContactInformationProfile : Profile
+    {
+        public ContactInformationProfile()
+        {
+            CreateMap<ContactInformation, ContactInformationDto>().ReverseMap();
+        }
+    }
+}
---
+++ Services/Identity/Atilim.Services.Identity.Application/Mapping/StudentIdentityProfiles/StudentIdentityProfile.cs
@@ -0,0 +1,16 @@
+﻿using Atilim.Services.Identity.Application.Dtos.StudentIdentityDtos;
+using Atilim.Services.Identity.Domain.Entities.StudentEntities;
+using AutoMapper;
+
+namespace Atilim.Services.Identity.Application.Mapping.StudentIdentityProfiles
+{
+    public class StudentIdentityProfile : Profile
+    {
+        public StudentIdentityProfile()
+        {
+            CreateMap<StudentIdentity, StudentIdentityDto>().ReverseMap();
+
+            CreateMap<StudentIdentity, UpdateStudentIdentityDto>().ReverseMap();
+        }
+    }
+}
